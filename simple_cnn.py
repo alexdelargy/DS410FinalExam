bfd99cbf8571d686578733962a1c25e603a50f46
alexdelargy <59781025+alexdelargy@users.noreply.github.com>
2024-04-30 02:10:25 -0400

    Update simple_cnn.py

--- simple_cnn.py
+++ simple_cnn.py
@@ -29,55 +29,54 @@ class Net(nn.Module):
         x = self.fc1(x)
         return x
 
-if __name__ == '__main__':
-            
-    spark = SparkSession.builder \
-        .appName("examples") \
-        .config('spark.driver.memory', '2g') \
-        .getOrCreate()
+        
+spark = SparkSession.builder \
+    .appName("examples") \
+    .config('spark.driver.memory', '2g') \
+    .getOrCreate()
 
-    # Read in mnist_train.csv dataset
-    df = spark.read.option("inferSchema", "true").csv('mnist_train.csv').orderBy(rand()).repartition(2)
+# Read in mnist_train.csv dataset
+df = spark.read.option("inferSchema", "true").csv('mnist_train.csv').orderBy(rand()).repartition(2)
 
-    network = Net()
+network = Net()
 
-    # Build the pytorch object
-    torch_obj = serialize_torch_obj(
-        model=network,
-        criterion=nn.CrossEntropyLoss(),
-        optimizer=torch.optim.Adam,
-        lr=0.001
-    )
+# Build the pytorch object
+torch_obj = serialize_torch_obj(
+    model=network,
+    criterion=nn.CrossEntropyLoss(),
+    optimizer=torch.optim.Adam,
+    lr=0.001
+)
 
-    # Setup features
-    vector_assembler = VectorAssembler(inputCols=df.columns[1:785], outputCol='features')
+# Setup features
+vector_assembler = VectorAssembler(inputCols=df.columns[1:785], outputCol='features')
 
-    # Demonstration of some options. Not all are required
-    # Note: This uses the barrier execution mode, which is sensitive to the number of partitions
-    spark_model = SparkTorch(
-        inputCol='features',
-        labelCol='_c0',
-        predictionCol='predictions',
-        torchObj=torch_obj,
-        iters=50,
-        verbose=1,
-        validationPct=0.2,
-        miniBatch=128
-    )
+# Demonstration of some options. Not all are required
+# Note: This uses the barrier execution mode, which is sensitive to the number of partitions
+spark_model = SparkTorch(
+    inputCol='features',
+    labelCol='_c0',
+    predictionCol='predictions',
+    torchObj=torch_obj,
+    iters=50,
+    verbose=1,
+    validationPct=0.2,
+    miniBatch=128
+)
 
-    # Create and save the Pipeline
-    p = Pipeline(stages=[vector_assembler, spark_model]).fit(df)
-    p.write().overwrite().save('simple_cnn')
+# Create and save the Pipeline
+p = Pipeline(stages=[vector_assembler, spark_model]).fit(df)
+p.write().overwrite().save('simple_cnn')
 
-    # Example of loading the pipeline
-    loaded_pipeline = PysparkPipelineWrapper.unwrap(PipelineModel.load('simple_cnn'))
+# Example of loading the pipeline
+loaded_pipeline = PysparkPipelineWrapper.unwrap(PipelineModel.load('simple_cnn'))
 
-    # Run predictions and evaluation
-    predictions = loaded_pipeline.transform(df).persist()
+# Run predictions and evaluation
+predictions = loaded_pipeline.transform(df).persist()
 
-    evaluator = MulticlassClassificationEvaluator(
-        labelCol="_c0", predictionCol="predictions", metricName="accuracy")
+evaluator = MulticlassClassificationEvaluator(
+    labelCol="_c0", predictionCol="predictions", metricName="accuracy")
 
-    accuracy = evaluator.evaluate(predictions)
-    print("Train accuracy = %g" % accuracy)
+accuracy = evaluator.evaluate(predictions)
+print("Train accuracy = %g" % accuracy)
 
